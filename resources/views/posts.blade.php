081df4b6c0f8be2a76cb69e524c3cb3ed4567a6b
ichsan <ichsan.alam01@gmail.com>
2024-12-30 13:03:45 +0700

    make post category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
             <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post['title'] }}</h2>
         </a>
         <div class="text-base text-gray-500">
-            <a href="/author/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 1 January 2024
+            <a href="/author/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | <a href="/category/{{ $post->category->slug }}" class="hover:underline">{{ $post->category->name }}</a> | {{ $post->created_at->toDayDateTimeString() }}
         </div>
         <p class="my-4 font-light">
             {{ Str::limit($post['body'], 100) }}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -28,6 +29,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/author/{user}', function (User $user) {
+Route::get('/author/{user:username}', function (User $user) {
     return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/category/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Categories by ' . $category->name, 'posts' => $category->posts]);
 });
